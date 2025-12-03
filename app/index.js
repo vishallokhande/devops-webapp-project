491e3626888afdc7cfe7e576b6d6edee08b4e3cd
vishal L <84737683+vishallokhande@users.noreply.github.com>
2025-12-03 18:16:16 +0530

    Add files via upload
    working file 1

---
+++ app/index.js
@@ -0,0 +1,5 @@
+t express = require('express');
+const app = express();
+app.get('/', (req,res)=> res.send('Hello from DevOps Project!'));
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> console.log('listening', port));
